e16a246c7f85c212db48ba8ea41374c59132f3bf
ADUCOUDRE <antoine.ducoudre@ynov.com>
2023-12-14 15:29:49 +0100

    test

--- dao/EtudiantDAO.py
+++ dao/EtudiantDAO.py
@@ -11,28 +11,101 @@ class EtudiantDAO(ModelDAO):
 
     def insert(self, objIns) -> int:
         try:
-            self.cursor.execute("INSERT INTO etudiant (id, matriculeetu, notes)
+            self.cursor.execute("INSERT INTO etudiant (id, matriculeetu, notes")
+            self.cursor.execute("VALUES (%s, %s, %s)", (objIns.getId(), objIns.getMatriculeEtu(), objIns.getNotes()))
+            return self.cursor.rowcount
+        except Exception as e:
+            print(e)
+            return 0
+        finally:
+            self.cursor.close()
+
 
     def insertList(self, objInsList) -> int:
-        pass
+        try:
+            self.cursor.executemany("INSERT INTO etudiant (id, matriculeetu, notes")
+            self.cursor.executemany("VALUES (%s, %s, %s)", objInsList)
+            return self.cursor.rowcount
+        except Exception as e:
+            print(e)
+            return 0
+        finally:
+            self.cursor.close()
+
 
     def findOne(self, findKey) -> object:
-        pass
+        try:
+            self.cursor.execute("SELECT * FROM etudiant WHERE matriculeetu = %s", (findKey,))
+            result = self.cursor.fetchone()
+            return Etudiant(result[0], result[1], result[2])
+        except Exception as e:
+            print(e)
+            return None
+        finally:
+            self.cursor.close()
 
     def findAll(self) -> list:
-        pass
+        try:
+            self.cursor.execute("SELECT * FROM etudiant")
+            result = self.cursor.fetchall()
+            etudiants = []
+            for row in result:
+                etudiants.append(Etudiant(row[0], row[1], row[2]))
+            return etudiants
+        except Exception as e:
+            print(e)
+            return None
+        finally:
+            self.cursor.close()
 
     def findAllByOne(self, findKey) -> list:
-        pass
+        try:
+            self.cursor.execute("SELECT * FROM etudiant WHERE matriculeetu = %s", (findKey,))
+            result = self.cursor.fetchall()
+            etudiants = []
+            for row in result:
+                etudiants.append(Etudiant(row[0], row[1], row[2]))
+            return etudiants
+        except Exception as e:
+            print(e)
+            return None
+        finally:
+            self.cursor.close()
 
     def findAllByLike(self, findKey) -> list:
-        pass
+        try:
+            self.cursor.execute("SELECT * FROM etudiant WHERE matriculeetu LIKE %s", (findKey,))
+            result = self.cursor.fetchall()
+            etudiants = []
+            for row in result:
+                etudiants.append(Etudiant(row[0], row[1], row[2]))
+            return etudiants
+        except Exception as e:
+            print(e)
+            return None
+        finally:
+            self.cursor.close()
 
     def update(self, cleAnc, objUpd) -> int:
-        pass
+        try:
+            self.cursor.execute("UPDATE etudiant SET notes = %s WHERE matriculeetu = %s", (objUpd.getNotes(), cleAnc))
+            return self.cursor.rowcount
+        except Exception as e:
+            print(e)
+            return 0
+        finally:
+            self.cursor.close()
+
 
     def deleteOne(self, cleSup) -> int:
-        pass
+        try:
+            self.cursor.execute("DELETE FROM etudiant WHERE matriculeetu = %s", (cleSup,))
+            return self.cursor.rowcount
+        except Exception as e:
+            print(e)
+            return 0
+        finally:
+            self.cursor.close()
 
     def createUser(self, pwd, user) -> int:
         pass
